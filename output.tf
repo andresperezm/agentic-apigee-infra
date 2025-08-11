c6e3ccc8404722a105b188343041009dca8221c8
Andres Perez <andresperezm@google.com>
2025-08-11 09:38:48 -0500

    Adds mcp_server_sa module outputs to root module outputt

--- output.tf
+++ output.tf
@@ -5,3 +5,11 @@ output "datastore_id" {
 output "datastore_location" {
     value = module.datastore.datastore_location
 }
+
+output "mcp_server_sa_email" {
+  value = module.mcp_server_sa.mcp_server_sa_email
+}
+
+output "mcp_server_deploy_sa_email" {
+    value = module.mcp_server_sa.mcp_server_deploy_sa_email
+}
